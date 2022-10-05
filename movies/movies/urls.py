c45d05102049ef9dbafdb39ff19fc3040d3b15ab
ariop <ariop@inacap.cl>
2022-10-05 16:04:33 -0300

    Finish 05/07/2022

--- movies/movies/urls.py
+++ movies/movies/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from movie.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',)
+    path('', home)
 ]

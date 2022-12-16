0dbf5c0a11f1bd0b9b300e9bbf769c4397dac949
unknown <diego2002lml@gmail.com>
2022-12-16 01:03:21 -0300

    ??

--- Biblioteca_Formativa/Libros/views.py
+++ Biblioteca_Formativa/Libros/views.py
@@ -1,20 +1,32 @@
 from django.shortcuts import render
-
 from django import forms
 from django.forms import ModelForm
 from .models import *
+from django.views.generic.list import ListView
 
 # Create your views here.
 
-class BibliotecaForm(ModelForm):
+#class BookForm(ModelForm):
 
-    class Meta:
+    #class Meta:
 
-        model = Book
-        fields = ['isbn', 'title', 'year', 'description', 'category', 'author']
+        #model = Book
+        #fields = ['isbn', 'title', 'year', 'description', 'category', 'author']
 
+
+class bookListView(ListView):
+    model = Book
 
 def home(request):
 
+    return render(request, 'Libros/templates/home.html')
 
-    return render(request)
+
+#def book_list(request):
+
+    #listado = Book.objects.all()
+
+    #context = {"context": listado}
+
+
+    #return render(request, "Libros/templates/book_list.html", context)
